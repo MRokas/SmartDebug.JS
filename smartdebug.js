d882c7b8b0a47d9ab7a2c92628eebc5b90a0fced
MRokas <rokasmuningis@gmail.com>
2016-03-13 14:35:13 +0200

    Support user-provided functions

--- smartdebug.js
+++ smartdebug.js
@@ -7,12 +7,19 @@
     else this[name] = definition();
 }('tryit', function() {
     function tryit(fn, con) {
-        var con = typeof con !== 'undefined' ? con : tryit.con;
+    con = typeof con !== undefined ? con : tryit.con;
+    con = typeof con === 'boolean'
+        ? con 
+            ? console.log.bind(console)
+            : false
+        : typeof con === 'function'
+            ? con
+            : false;
     	try {
     		fn();
     	} catch(ex) {
     		var link = ("http://stackoverflow.com/search?q=[js] + " + ex.message);
-    		!con ? (window.location.href = link) : (console.log(link));
+    		!con ? (window.location.href = link) : (con(link));
     	}
     }
     tryit.con = false;
